880e0612e6e1cfb78d9f66b7ffcdf89f90af6f8d
Jenny Abella <jenny.abella1@gmail.com>
2018-02-12 19:40:19 -0800

    This is the real version of the master template

---
+++ main.asm
@@ -0,0 +1,34 @@
+; Assembler Linux Template
+; Program Description:
+
+; Author:
+
+; Creation Date:
+
+; Revisions: 
+
+; Date:              Modified by:
+
+; Operating System:
+
+; IDE/Compiler:
+
+bbits 32
+section .data
+  ;Initialized data definitions go here
+
+section .bss
+  ;Uninitialized memory reservations go here
+
+section .text
+
+global _start
+_start:
+  nop
+  ;Code starts here
+
+  ;Code ends here
+  nop
+  mov eax,1 ; Exit system call value
+  mov ebx,0 ; Exit return code
+  int 80h ; Call the kernel
